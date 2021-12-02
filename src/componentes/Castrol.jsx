a11f86a28440258a3167301b19b5e998a5ba6d13
Yajaira Marin <jeyamaol@hotmail.com>
2021-12-02 18:30:02 -0500

    master

--- src/componentes/Castrol.jsx
+++ src/componentes/Castrol.jsx
@@ -1,2 +1,3 @@
 import React, {useState} from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/../../node_modules/bootstrap/dist/js/bootstrap.min.css'
