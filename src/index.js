8e0cef64d2f652e08f99cd7cc6bd1fe007c7978e
Yajaira Marin <jeyamaol@hotmail.com>
2021-12-02 17:26:45 -0500

    master

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App2 from './App2';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
