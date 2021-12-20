8ceb542e84f6376230f2803f503e99b5f2e87a6e
Robertico <robert.624@hotmail.com>
2021-12-20 00:57:23 -0500

    Agregar useStates

--- src/componentes/ingresar/Login.js
+++ src/componentes/ingresar/Login.js
@@ -1,73 +1,104 @@
-import { Link} from "react-router-dom";
-import Menu from '../menu/Menu'; 
-import Footer from '../footer/Footer'; 
- 
+import { Link } from "react-router-dom";
+import Menu from "../menu/Menu";
+import Footer from "../footer/Footer";
+import Axios from "axios";
+import { useState } from "react";
 
-  function Login() {       
-        return (
-            <>
+function Login() {
+	const [emailUser, setEmailUser] = useState("");
 
-            <Menu />
-            
-            <div className="container">
-                <div className="row justify-content-center">
-        
-                    <div className="col-xl-10 col-lg-10 col-md-9">
-        
-                        <div className="card o-hidden border-0 shadow-lg my-6">
-                            <div className="card-body p-0">
-                                <div className="row">
-                                    <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                                    <div className="col-lg-6">
-                                        <div className="p-5">
-                                            <div className="text-center">
-                                                <h1 className="h4 text-gray-900 mb-4">¡Bienvenido de nuevo!</h1>
-                                            </div>
-                                            <form className="user">
-                                                <div className="form-group">
-                                                    <input type="email" className="form-control form-control-user"
-                                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                                        placeholder="Ingrese su correo" />
-                                                </div>
-                                                <div className="form-group">
-                                                    <input type="password" className="form-control form-control-user"
-                                                        id="exampleInputPassword" placeholder="Contraseña" />
-                                                </div>
-                                                <div className="form-group">
-                                                    <div className="custom-control custom-checkbox small">
-                                                        <input type="checkbox" className="custom-control-input" id="customCheck" />
-                                                        <label className="custom-control-label" for="customCheck">Recuérdame</label>
-                                                    </div>
-                                                </div>
-                                                <Link to="/App" className="btn btn-primary btn-user btn-block">
-                                                    Ingresar
-                                                </Link>                                           
-                                            </form>
+	const loguearse = () => {
+		Axios.post("localhost:8080/auth/login", {
+			email: emailUser,
+			password: "monijkjkca",
+		});
+	};
 
-                                            <div className="text-center">
-                                                <Link className="small" to="/Recuperar">¿Olvidaste tu contraseña?</Link>
-                                            </div>
-                                            <div className="text-center">
-                                                <Link className="small" to="/registrar">¡Crea tu cuenta!</Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-        
-                    </div>
-        
-                </div>  
-    
-        </div>
+	return (
+		<>
+			<Menu />
 
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-xl-10 col-lg-10 col-md-9">
+						<div className="card o-hidden border-0 shadow-lg my-6">
+							<div className="card-body p-0">
+								<div className="row">
+									<div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+									<div className="col-lg-6">
+										<div className="p-5">
+											<div className="text-center">
+												<h1 className="h4 text-gray-900 mb-4">
+													¡Bienvenido de nuevo!
+												</h1>
+											</div>
+											<form className="user">
+												<div className="form-group">
+													<input
+														type="email"
+														className="form-control form-control-user"
+														id="exampleInputEmail"
+														aria-describedby="emailHelp"
+														placeholder="Ingrese su correo"
+														onChange={(e) => {
+															setEmailUser(e.target.value);
+														}}
+													/>
+												</div>
+												<h1>{emailUser}</h1>
+												<div className="form-group">
+													<input
+														type="password"
+														className="form-control form-control-user"
+														id="exampleInputPassword"
+														placeholder="Contraseña"
+													/>
+												</div>
+												<div className="form-group">
+													<div className="custom-control custom-checkbox small">
+														<input
+															type="checkbox"
+															className="custom-control-input"
+															id="customCheck"
+														/>
+														<label
+															className="custom-control-label"
+															for="customCheck"
+														>
+															Recuérdame
+														</label>
+													</div>
+												</div>
+												<button
+													onClick={loguearse}
+													className="btn btn-primary btn-user btn-block"
+												>
+													Ingresar
+												</button>
+											</form>
 
-        
-    <Footer />
-    </>
+											<div className="text-center">
+												<Link className="small" to="/Recuperar">
+													¿Olvidaste tu contraseña?
+												</Link>
+											</div>
+											<div className="text-center">
+												<Link className="small" to="/registrar">
+													¡Crea tu cuenta!
+												</Link>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 
-    );
-  }
+			<Footer />
+		</>
+	);
+}
 
-  export default Login;
+export default Login;
--- src/componentes/registrar/Registrar.js
+++ src/componentes/registrar/Registrar.js
@@ -1,78 +1,119 @@
-import { Link} from "react-router-dom";
-import Menu from '../menu/Menu'; 
-import Footer from '../footer/Footer'; 
- 
+import { Link } from "react-router-dom";
+import Menu from "../menu/Menu";
+import Footer from "../footer/Footer";
+import Axios from "axios";
 
-  function Registrar() {       
-        return (
-            <>
-
+function Registrar() {
+	const registrarse = () => {
+		Axios.post("localhost:8080/auth/register", {
+			name: "Robert",
+			lastname: "ramirez",
+			age: 25,
+			email: "robert@hotmail.com",
+			password: "america",
+			birthDay: "1996-05-29",
+		});
+	};
+	return (
+		<>
 			<Menu />
-            <div className="container">
-                <div className="row justify-content-center">    
-                    <div className="col-xl-10 col-lg-10 col-md-9">
-                    <div className="card o-hidden border-0 shadow-lg my-6">
-                        <div className="card-body p-0">
-                            {/* <!-- Nested Row within Card Body --> */}
-                            <div className="row">
-                                <div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                                <div className="col-lg-7">
-                                    <div className="p-5">
-                                        <div className="text-center">
-                                            <h1 className="h4 text-gray-900 mb-4">¡Crea tu cuenta!</h1>
-                                        </div>
-                                        <form className="user">
-                                            <div className="form-group row">
-                                                <div className="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="text" className="form-control form-control-user" id="exampleFirstName"
-                                                        placeholder="Nombre" />
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <input type="text" className="form-control form-control-user" id="exampleLastName"
-                                                        placeholder="Apellido" />
-                                                </div>
-                                            </div>
-                                            <div className="form-group">
-                                                <input type="email" className="form-control form-control-user" id="exampleInputEmail"
-                                                    placeholder="Correo" />
-                                            </div>
-                                            <div className="form-group">
-                                                <input type="email" className="form-control form-control-user" id="exampleInputEmail"
-                                                    placeholder="Confirmar correo" />
-                                            </div>
-                                            <div className="form-group row">
-                                                <div className="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="password" className="form-control form-control-user"
-                                                        id="exampleInputPassword" placeholder="Contraseña" />
-                                                </div>
-                                                <div className="col-sm-6">
-                                                    <input type="password" className="form-control form-control-user"
-                                                        id="exampleRepeatPassword" placeholder="Confirmar contraseña" />
-                                                </div>
-                                            </div>
-                                            <Link to="/Login" className="btn btn-primary btn-user btn-block">
-                                                Registrarse
-                                            </Link>
-                                        </form>
-                                    
-                                        <div className="text-center">
-                                                <Link className="small" to="/Recuperar">¿Olvidaste tu contraseña?</Link>
-                                        </div>
-                                        <div className="text-center">
-                                            <Link className="small" to="/Login">¿Ya tienes una cuenta? ¡Ingresa!</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-             </div>
-            
-        </div>
-        <Footer />
-            </>
-        );
-  }
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-xl-10 col-lg-10 col-md-9">
+						<div className="card o-hidden border-0 shadow-lg my-6">
+							<div className="card-body p-0">
+								{/* <!-- Nested Row within Card Body --> */}
+								<div className="row">
+									<div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
+									<div className="col-lg-7">
+										<div className="p-5">
+											<div className="text-center">
+												<h1 className="h4 text-gray-900 mb-4">
+													¡Crea tu cuenta!
+												</h1>
+											</div>
+											<form className="user">
+												<div className="form-group row">
+													<div className="col-sm-6 mb-3 mb-sm-0">
+														<input
+															type="text"
+															className="form-control form-control-user"
+															id="exampleFirstName"
+															placeholder="Nombre"
+														/>
+													</div>
+													<div className="col-sm-6">
+														<input
+															type="text"
+															className="form-control form-control-user"
+															id="exampleLastName"
+															placeholder="Apellido"
+														/>
+													</div>
+												</div>
+												<div className="form-group">
+													<input
+														type="email"
+														className="form-control form-control-user"
+														id="exampleInputEmail"
+														placeholder="Correo"
+													/>
+												</div>
+												<div className="form-group">
+													<input
+														type="email"
+														className="form-control form-control-user"
+														id="exampleInputEmail"
+														placeholder="Confirmar correo"
+													/>
+												</div>
+												<div className="form-group row">
+													<div className="col-sm-6 mb-3 mb-sm-0">
+														<input
+															type="password"
+															className="form-control form-control-user"
+															id="exampleInputPassword"
+															placeholder="Contraseña"
+														/>
+													</div>
+													<div className="col-sm-6">
+														<input
+															type="password"
+															className="form-control form-control-user"
+															id="exampleRepeatPassword"
+															placeholder="Confirmar contraseña"
+														/>
+													</div>
+												</div>
+												<button
+													onClick={registrarse}
+													className="btn btn-primary btn-user btn-block"
+												>
+													Registrarse
+												</button>
+											</form>
 
-  export default Registrar;
+											<div className="text-center">
+												<Link className="small" to="/Recuperar">
+													¿Olvidaste tu contraseña?
+												</Link>
+											</div>
+											<div className="text-center">
+												<Link className="small" to="/Login">
+													¿Ya tienes una cuenta? ¡Ingresa!
+												</Link>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
+
+export default Registrar;
